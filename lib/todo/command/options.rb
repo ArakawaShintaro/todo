417f6f6a2dbf53bbfdb268121da1132e3df84b37
Shintaro Arakawa <300753@MBPR0030.local>
2017-11-28 17:20:31 +0900

    ٩( 'ω' )و

--- lib/todo/command/options.rb
+++ lib/todo/command/options.rb
@@ -6,15 +6,50 @@ module Todo
     module Options
 
       def self.parse!(argv)
-        command_parser = OptionParser.new do |opt|
+        options = {}
+
+        sub_commands_parsers = create_sub_command_parsers(options)
+        command_parser = create_command_parser
+
+        begin
+          command_parser.order!(argv)
+          options[:command] = argv.shift
+          sub_commands_parsers[options[:command]].parse!(argv)
+        rescue OptionParser::MissingArgument, OptionParser::InvalidOption, ArgumentError => e
+          abort e.message
+        end
+      end
+
+      def self.create_sub_command_parsers(optsions)
+         sub_commands_parsers = Hash.new do |k ,v|
+          raise ArgumentError, "#{v} is not todo sub command"
+        end
+
+        sub_commands_parsers['create'] = OptionParser.new do |opt|
+          opt.on('-n VAL', '--name=VAL', 'task name') { |v| options[:name] = v }
+          opt.on('-c VAL', '--content=VAL', 'task content') { |v| options[:content] = v }
+        end
+
+        sub_commands_parsers['list'] = OptionParser.new do |opt|
+          opt.on('-s VAL', '--status=VAL', 'list status') { |v| options[:status] = v }
+        end
+
+        sub_commands_parsers['create'] = OptionParser.new do |opt|
+          opt.on('-n VAL', '--name=VAL', 'task name') { |v| options[:name] = v }
+          opt.on('-c VAL', '--content=VAL', 'task content') { |v| options[:content] = v }
+        end
+
+        sub_commands_parsers
+      end
+
+      def self.create_command_parser
+        OptionParser.new do |opt|
           opt.on_head('-v', '--version', 'Show program version') do |v|
             opt.version = Todo::VERSION
             puts opt.version
             exit
           end
         end
-
-        command_parser.parse!(argv)
       end
 
     end
